4f7a8f4e39f4c4fa1a232fc77e2eeeaec50860fb
ShyamGuna77 <gshyamprasad2001@gmail.com>
2026-01-21 16:22:44 +0000

    wip

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+
 
 func main() {
 	fmt.Println("Hello, Go project!")
+}
+
+
+
+
+//middleware runs before handlers 
+
+// middleware takes a http.Handler and returns a httphndler
+
+// it also returns a http.HandlerFunc which takes resp (w) and req (r) 
+
+// we return middleware usng next like next.ServeHttp(w,r)
+
+
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+
+   return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	log.Println("Before server started")
+	next.ServeHTTP(w,r)
+	log.Println("After server started")
+   })
 }
