07fa09817cb87a2d72c61a2f17b997814fa2084e
ShyamGuna77 <gshyamprasad2001@gmail.com>
2026-01-22 11:18:39 +0000

    fix

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,25 +4,40 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from Server "))
+	w.Header().Add("Server", "Go")
+	w.Write([]byte("This is Home  snippet box"))
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("This is a snippet viewer box"))
+
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("This is a snippet creater  box"))
 }
+
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("THis is a Post request"))
+}
+
 func main() {
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/{$}", home)
-	mux.HandleFunc("/view", snippetView)
-	mux.HandleFunc("/create", snippetCreate)
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 	port := ":3000"
 
 	fmt.Println("server started on :", port)
