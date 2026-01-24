cb24bc980ec1344cb7464bc85745e12de3d8b5fe
ShyamGuna77 <gshyamprasad2001@gmail.com>
2026-01-24 09:15:13 +0000

    feat: add MySQL database connection and update go.mod/go.sum

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"log/slog"
@@ -8,24 +9,44 @@ import (
 	"os"
 
 	"github.com/ShyamGuna77/rest-sms/internal/web"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
-
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+	logger.Info("database connection pool established")
 	app := &web.Application{
 		Logger: logger,
 	}
-	
 
 	logger.Info("server started on :", "addr", *addr)
 
 	router := app.Routes()
 
-	err := http.ListenAndServe(*addr, router)
+	err = http.ListenAndServe(*addr, router)
 	log.Fatal(err)
 
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+	return db, nil
 }
