892945df5758c3081f5918908cfc280447e1e491
ShyamGuna77 <gshyamprasad2001@gmail.com>
2026-02-11 09:10:29 +0000

    wip

--- internal/web/handlers.go
+++ internal/web/handlers.go
@@ -27,7 +27,7 @@ func (app *Application) Home(w http.ResponseWriter, r *http.Request) {
 	data := TemplateData{
 		Snippets: snippets,
 	}
-	app.render(w, r, http.StatusOK, "home.html", data)
+	app.render(w, r, http.StatusOK, "home.tmpl", data)
 }
 func (app *Application) SnippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
--- internal/web/helpers.go
+++ internal/web/helpers.go
@@ -1,7 +1,6 @@
 package web
 
 import (
-	"bytes"
 	"fmt"
 
 	"net/http"
@@ -28,18 +27,22 @@ func (app *Application) NotFound(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *Application) render(w http.ResponseWriter, r *http.Request, status int, page string, data TemplateData) {
+	// Retrieve the appropriate template set from the cache based on the page
+	// name (like 'home.tmpl'). If no entry exists in the cache with the
+	// provided name, then create a new error and call the serverError() helper
+	// method that we made earlier and return.
 	ts, ok := app.TemplateCache[page]
 	if !ok {
 		err := fmt.Errorf("the template %s does not exist", page)
 		app.ServerError(w, r, err)
 		return
 	}
-	buf := new(bytes.Buffer)
-	err := ts.ExecuteTemplate(buf, "base", data)
+	// Write out the provided HTTP status code ('200 OK', '400 Bad Request' etc).
+	w.WriteHeader(status)
+	// Execute the template set and write the response body. Again, if there
+	// is any error we call the serverError() helper.
+	err := ts.ExecuteTemplate(w, "base", data)
 	if err != nil {
 		app.ServerError(w, r, err)
-		return
 	}
-	w.WriteHeader(status)
-	buf.WriteTo(w)
 }
