406899c0a47d02502473cc30a4a6e24383eec9b7
ShyamGuna77 <gshyamprasad2001@gmail.com>
2026-02-23 09:58:04 +0000

    feat:add user signup auth

--- internal/web/handlers.go
+++ internal/web/handlers.go
@@ -122,9 +122,59 @@ func (app *Application) userSignup(w http.ResponseWriter, r *http.Request) {
     data.Form = userSignupForm{}
     app.render(w, r, http.StatusOK, "signup.html", data)	
 }
+
+
 func (app *Application) userSignupPost(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Create a new user...")
+    // Declare a zero-valued instance of our userSignupForm struct.
+    var form userSignupForm
+    // Parse the form data into the userSignupForm struct.
+    err := app.DecodeForm(r, &form)
+    if err != nil {
+        app.ClientError(w, http.StatusBadRequest)
+        return
+    }
+    // Validate the form contents using our helper functions.
+    form.CheckField(validator.NotBlank(form.Name), "name", "This field cannot be blank")
+    form.CheckField(validator.NotBlank(form.Email), "email", "This field cannot be blank")
+    form.CheckField(validator.Matches(form.Email, validator.EmailRX), "email", "This field must be a valid email address")
+    form.CheckField(validator.NotBlank(form.Password), "password", "This field cannot be blank")
+    form.CheckField(validator.MinChars(form.Password, 8), "password", "This field must be at least 8 characters long")
+    // If there are any errors, redisplay the signup form along with a 422
+    // status code.
+    if !form.Valid() {
+        data := app.newTemplateData(r)
+        data.Form = form
+        app.render(w, r, http.StatusUnprocessableEntity, "signup.html", data)
+        return
+    }
+     // Try to create a new user record in the database. If the email already
+    // exists then add an error message to the form and redisplay it.
+    err = app.Users.Insert(form.Name, form.Email, form.Password)
+    if err != nil {
+        if errors.Is(err, models.ErrDuplicateEmail) {
+            form.AddFieldError("email", "Email address is already in use")
+            data := app.newTemplateData(r)
+            data.Form = form
+            app.render(w, r, http.StatusUnprocessableEntity, "signup.html", data)
+        } else {
+            app.ServerError(w, r, err)
+        }
+        return
+    }
+    // Otherwise add a confirmation flash message to the session confirming that
+    // their signup worked.
+    app.SessionManager.Put(r.Context(), "flash", "Your signup was successful. Please log in.")
+    // And redirect the user to the login page.
+    http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 }
+
+
+
+
+
+
+
+
 func (app *Application) userLogin(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintln(w, "Display a form for logging in a user...")
 }
